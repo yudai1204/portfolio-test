dc716e3c4d449bcecbf483fa91cd8c6ea6acbe64
yudai1204 <yudai1204h@gmail.com>
2023-06-13 17:23:45 +0900

    aaaa

--- components/menu.js
+++ components/menu.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import styles from '../styles/Menu.module.css';
 
-export default function Menu({active}) {
+export default function Menu(props) {
     return (
         <div className={styles.menu}>
             <div className={styles.icons}>
-                <img src="https://yudai04.dev/icon.jpg" />
+                <a href="https://yudai04.dev/">
+                    <img src="https://yudai04.dev/icon.jpg" />
+                </a>
             </div>
             <ul>
                 <li className={styles.menuLi}><Link href="/">Home</Link></li>
